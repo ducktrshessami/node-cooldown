9da6c71e781649926b9ea120dc78afd38071895e
ducktrshessami <jwee761@gmail.com>
2022-03-28 21:25:37 -0700

    implement ready property

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 function cooldown(callback, ms = 0) {
     let ready = true;
-    return function (...args) {
+    let wrapped = function (...args) {
         if (ready) {
             ready = false;
             setTimeout(() => ready = true, ms);
             return callback.apply(this, args);
         }
-    }
+    };
+    Object.defineProperty(wrapped, "ready", {
+        get: () => ready,
+        enumerable: true
+    });
+    return wrapped;
 }
 
 // Always async
 function cooldownAsync(callback, ms = 0) {
     let ready = true;
-    return async function (...args) {
+    let wrapped = async function (...args) {
         if (ready) {
             ready = false;
             setTimeout(() => ready = true, ms);
             return callback.apply(this, args);
         }
-    }
+    };
+    Object.defineProperty(wrapped, "ready", {
+        get: () => ready,
+        enumerable: true
+    });
+    return wrapped;
 }
 
 Object.defineProperties(module.exports, {
