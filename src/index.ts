64d700fac5b20f1d65ee5d8d6e051e3abc26066f
ducktrshessami <jwee761@gmail.com>
2023-09-07 20:12:13 -0700

    refactor: types

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,37 @@
-type Callback<T> = (...args: any[]) => T;
-type AsyncCallback<T> = (...args: any[]) => Promise<T>;
+type Callback<P = any, R = any> = (...args: P[]) => R;
+type Readiable = { ready: boolean };
+type CooldownFunction<C extends Callback> = Readiable & ((...args: Parameters<C>) => ReturnType<C> | void);
+type CooldownAsyncFunction<C extends Callback> = Readiable & ((...args: Parameters<C>) => Promise<ReturnType<C> | void>);
 
-interface CooldownFunction<T> extends Callback<T | void> {
-    ready: Boolean;
-}
-
-interface CooldownAsyncFunction<T> extends AsyncCallback<T | void> {
-    ready: Boolean;
-}
-
-export function cooldown<T>(callback: Callback<T>, ms: number = 0): CooldownFunction<T> {
+export function cooldown<C extends Callback>(callback: C, ms: number = 0): CooldownFunction<C> {
     let ready = true;
-    let wrapped = function (this: CooldownFunction<T>, ...args: Parameters<typeof callback>) {
+    function wrapped(this: unknown, ...args: Parameters<C>): ReturnType<C> | void {
         if (ready) {
             ready = false;
             setTimeout(() => ready = true, ms);
             return callback.apply(this, args);
         }
-    };
+    }
     Object.defineProperty(wrapped, "ready", {
         get: () => ready,
         enumerable: true
     });
-    return <CooldownFunction<T>>wrapped;
+    return <CooldownFunction<C>>wrapped;
 }
 
 // Always async
-export function cooldownAsync<T>(callback: AsyncCallback<T>, ms: number = 0): CooldownAsyncFunction<T> {
+export function cooldownAsync<C extends Callback>(callback: C, ms: number = 0): CooldownAsyncFunction<C> {
     let ready = true;
-    let wrapped = async function (this: CooldownFunction<T>, ...args: Parameters<typeof callback>) {
+    async function wrapped(this: unknown, ...args: Parameters<C>): Promise<ReturnType<C> | void> {
         if (ready) {
             ready = false;
             setTimeout(() => ready = true, ms);
-            return callback.apply(this, args);
+            return await callback.apply(this, args);
         }
-    };
+    }
     Object.defineProperty(wrapped, "ready", {
         get: () => ready,
         enumerable: true
     });
-    return <CooldownAsyncFunction<T>>wrapped;
+    return <CooldownAsyncFunction<C>>wrapped;
 }
